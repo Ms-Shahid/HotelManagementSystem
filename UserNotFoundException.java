33d61bcf276772806335e2f18bb90cafd4063d81
vasanthasriram98 <86831135+vasanthasriram98@users.noreply.github.com>
2021-10-17 21:57:34 +0530

    Add files via upload

---
+++ UserNotFoundException.java
@@ -0,0 +1,15 @@
+package com.cg.hbm.exception;
+
+
+
+public class UserNotFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public UserNotFoundException( String errorMessage) {
+		super(errorMessage);
+		}
+
+}
