3f6817b7f7961ad2c874631a2f38dca31b4788dd
Nanditha12D <nanditha.d5010@gmail.com>
2021-10-18 15:54:48 +0530

    Transactions

---
+++ src/main/java/com/example/Transactions/service/ITransactionsService.java
@@ -0,0 +1,5 @@
+package com.example.Transactions.service;
+import com.example.Transactions.entity.Transactions;
+public interface ITransactionsService {
+	public Transactions addTransaction(Transactions transaction);
+}
