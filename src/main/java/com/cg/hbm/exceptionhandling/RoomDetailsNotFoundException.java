123dc0c8b501fb98c262e257f933aa6267fd2345
Nanditha12D <nanditha.d5010@gmail.com>
2021-10-18 15:50:35 +0530

    Room Management

---
+++ src/main/java/com/cg/hbm/exceptionhandling/RoomDetailsNotFoundException.java
@@ -0,0 +1,15 @@
+package com.cg.hbm.exceptionhandling;
+
+
+
+public class RoomDetailsNotFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+
+	public RoomDetailsNotFoundException( String errorMessage) {
+		super(errorMessage);
+		}
+
+}
