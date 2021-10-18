31116f21802e095f4cad2934b875d2b018744a58
vasanthasriram98 <vasanthasriram98@gmail.com>
2021-10-18 15:36:33 +0530

    UserModule

---
+++ src/main/java/com/cg/hbm/exception/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.cg.hbm.exception;
+
+
+
+public class UserNotFoundException extends RuntimeException{
+
+	public UserNotFoundException( String errorMessage) {
+		super(errorMessage);
+		}
+
+}
