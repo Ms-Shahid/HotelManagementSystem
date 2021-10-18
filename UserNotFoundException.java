fc974149abb370765643850630a1eb115c7e387a
vasanthasriram98 <86831135+vasanthasriram98@users.noreply.github.com>
2021-10-18 14:44:35 +0530

    Update UserNotFoundException.java

--- UserNotFoundException.java
+++ UserNotFoundException.java
@@ -3,11 +3,6 @@ package com.cg.hbm.exception;
 
 
 public class UserNotFoundException extends RuntimeException{
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 	public UserNotFoundException( String errorMessage) {
 		super(errorMessage);
 		}
